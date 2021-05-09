8aee44099285b0b3d9ba4bdb25c2061e58e541e2
Xavier Nunez <xaviernunez83@gmail.com>
2021-05-09 11:53:15 -0700

    finish lab 4

--- lab_4.py
+++ lab_4.py
@@ -14,3 +14,21 @@ if the user could not guess the correct answer within the max_tries, then print
 "You have used up your allotment of guesses." then print the "Correct answer is Sacramento"
 """
 
+"""
+main 
+    question = "What is the capital of California"
+    answer = "Sacramento"
+    ask(question, answer)
+
+ask 
+    tries = 0 
+    loop three times 
+        increment tries by 1
+        ask user input()
+        check to see if user input is equal to answer
+            if so, print "Correct" the exit loop
+    if not correct
+        print to the user "You have used up your allotment of guesses."
+        print the correct answer "The correct answer is "Sacramento"
+main
+"""
